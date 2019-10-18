09ec5a72fc371eac0f937604d0258d7f3ab78e03
3RDD <3rdd@3rd-dimension.nz>
2019-10-18 20:36:11 +1300

    Changed which value updater uses to get Version number from Github
    Change which value Autoupdater gets the version from Github.  Uses tag_name instead of name from the json.

--- GCodeSender/MainWindow.xaml.cs
+++ GCodeSender/MainWindow.xaml.cs
@@ -102,7 +102,7 @@ namespace GCodeSender
         {
             dynamic json = JsonConvert.DeserializeObject(args.RemoteData);
 
-            string VersionGitHub = json.name; // Version Number - Change this to tag_name on deployment
+            string VersionGitHub = json.tag_name; // Version Number
             string AssetDownloadURL = "";
             VersionGitHub = (VersionGitHub.Remove(0, 1)); // Remove "v" from beginning
             Version v = new Version(VersionGitHub); // Conver to Version
